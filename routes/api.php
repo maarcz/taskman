f1b2a76c68e2141c2e13b242ae409c277914ecd8
Mārcis Zoldners <marcis.zoldners@gmail.com>
2018-07-22 13:09:19 +0300

    Setting up Laravel

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+Route::get('projects', 'ProjectController@index');
+Route::post('projects', 'ProjectController@store');
+Route::get('projects/{id}', 'ProjectController@show');
+Route::put('projects/{project}', 'ProjectController@markAsCompleted');
+Route::post('tasks', 'TaskController@store');
+Route::put('tasks/{task}', 'TaskController@markAsCompleted');
